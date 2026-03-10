a051e78a60ab3e9d44410be11f5e330f7a3a9146
CelereonTechnologies <fathogcor@gmail.com>
2026-03-09 19:41:36 -0700

    Update server.py

--- server.py
+++ server.py
@@ -5,25 +5,36 @@ import os
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return "Blackfyre API running"
+
 @app.route("/download")
 def download():
 
     url = request.args.get("url")
 
-    filename=str(uuid.uuid4())+".mp4"
+    if not url:
+        return "Missing video URL"
+
+    filename = str(uuid.uuid4()) + ".mp4"
 
     ydl_opts = {
-        'format': 'mp4',
+        'format': 'best',
         'outtmpl': filename
     }
 
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
 
-    response=send_file(filename, as_attachment=True)
+        return send_file(filename, as_attachment=True)
 
-    os.remove(filename)
+    except Exception as e:
+        return str(e)
 
-    return response
+    finally:
+        if os.path.exists(filename):
+            os.remove(filename)
 
 app.run(host="0.0.0.0", port=10000)
